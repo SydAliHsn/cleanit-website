debd73cbad7fe9769e2388aa2c835302359c8111
Syed Ali Hassan <syedalihassan6651@gmail.com>
2024-05-13 19:00:52 +0500

    Minor animation tweak.

--- app/page.tsx
+++ app/page.tsx
@@ -190,7 +190,7 @@ export default function Home() {
       <motion.section
         initial={{ opacity: 0, y: 100 }}
         whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.3 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
         viewport={{ once: true, amount: 0.2 }}
         id="cta">
         <div className="container">
